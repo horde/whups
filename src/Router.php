17e2f7d16e1ed03fcd5469448581ebb841771229
Ralf Lang <ralf.lang@ralf-lang.de>
2025-05-12 21:41:39 +0200

    fix: Use classmap autoloader. Whups is not strictly PSR-0

---
+++ src/Router.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+namespace Horde\Whups;
+
+/**
+ * Whups Router
+ *
+ * @category Horde
+ * @package  Whups
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ */
+class Router
+{
+}
