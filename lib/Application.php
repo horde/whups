4a0181c404a76fe41feeb08923298d4347aacad0
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-09 22:38:38 +0200

    chore(release): bump version to 4.0.0-beta2
    Release version 4.0.0-beta2
    
    fix: Multiple architecture violations with Horde:: or Horde_Deprecated:: calls
    fix: Various strftime architecture violations.
    fix: We want to skip to the next ticket, not to the end of the switch
    feat: Offload some expensive parts of report generation to driver
    feat: Cache generated reports.
    chore(hordeyml): Whups now depends on horde/cache and PSR cache interfaces
    style: php-cs-fixer
    feat: Cache for metadata which only changes on admin actions
    Merge pull request #6 from horde/feat/replace-horde-template
    feat: Replace Horde_Template with Horde_View
    fix: More defensive code when trying to drop tables
    docs: Clarify where to put local overrides
    chore: Use named params for clarity
    fix: Accept both types of variables
    fix(ticket/attachements.php): use ticket->getId() instead of undefined  variable

--- lib/Application.php
+++ lib/Application.php
@@ -42,7 +42,7 @@ class Whups_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = '4.0.0-beta1';
+    public $version = '4.0.0-beta2';
 
     /**
      * Global variables defined:
