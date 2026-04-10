818676a32abd532005b2fab4c69dcdeac5d8dcce
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-10 07:27:53 +0200

    chore(release): bump version to 4.0.0-beta3
    Release version 4.0.0-beta3
    
    test: upgrade unit test suite and add contract tests for some pages
    refactor: Replace Horde::url with explicit calls to registry and Horde_Url or use router
    refactor: turn some endpoints into thin shims around controllers
    feat: Begin refactoring opensearch, queue view and rss endpoints into controllers
    refactor: Use horde routes mapper and always prefer the pretty route
    feat: Onboard all routes of whups to the routes file

--- lib/Application.php
+++ lib/Application.php
@@ -42,7 +42,7 @@ class Whups_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = '4.0.0-beta2';
+    public $version = '4.0.0-beta3';
 
     /**
      * Global variables defined:
