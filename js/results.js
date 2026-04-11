1824b980de755f90e1e0aeb10242d256b3131dbf
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-11 08:45:42 +0200

    refactor: Convert prototypejs to vanilla js

--- js/results.js
+++ js/results.js
@@ -1,7 +1,7 @@
 /**
  * JavaScript for result lists.
  *
- * Copyright 2016-2017 Horde LLC (http://www.horde.org/)
+ * Copyright 2016-2026 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (BSD). If you
  * did not receive this file, see http://www.horde.org/licenses/bsdl.php.
@@ -13,13 +13,18 @@ function table_sortCallback(tableId, column, sortDown)
     HordeCore.doAction('setPrefValue', { pref: 'sortdir', value: sortDown });
 }
 
-document.observe('dom:loaded', function() {
-    $('check-all').observe('click', function() {
-        var inputs = $('delete-form').getInputs('checkbox', 'ticket[]'),
-            check = inputs.any(function(c) { return !c.checked; });
-        inputs.each(function(input) {
-            input.checked = check;
+document.addEventListener('DOMContentLoaded', function() {
+    var checkAll = document.getElementById('check-all');
+    if (checkAll) {
+        checkAll.addEventListener('click', function() {
+            var inputs = Array.from(
+                document.getElementById('delete-form').querySelectorAll('input[type="checkbox"][name="ticket[]"]')
+            );
+            var check = inputs.some(function(c) { return !c.checked; });
+            inputs.forEach(function(input) {
+                input.checked = check;
+            });
+            checkAll.checked = check;
         });
-        $('check-all').checked = check;
-    });
+    }
 });
