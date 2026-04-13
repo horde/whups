2be0b236e05d4c77ffd55947f8a1eed1fb81a9ea
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-13 14:36:15 +0200

    refactor: Support whups as the entrypoint script

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
 
-if (is_dir(dirname(__FILE__, 3) . '/vendor')) {
-    require_once dirname(__FILE__, 3) . '/vendor/autoload.php';
-} elseif (is_dir(dirname(__FILE__, 4) . '/vendor')) {
-    require_once dirname(__FILE__, 4) . '/vendor/autoload.php';
-}
-Horde\Core\RampageBootstrap::run();
+require __DIR__ . '/rampage.php';
