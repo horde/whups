211ea45940eeac56f8ebc2071514958e0638acda
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-24 08:17:21 +0200

    docs(UrlGenerator): comment future migration to RouteUrlWriter
    Addresses issue horde/Core#121

--- src/Service/UrlGenerator.php
+++ src/Service/UrlGenerator.php
@@ -23,6 +23,12 @@ namespace Horde\Whups\Service;
 use Horde\Routes\Mapper;
 use Horde\Routes\Utils;
 
+/**
+ * NOTE: New code should use Horde\Core\Uri\RouteUrlWriter instead.
+ * RouteUrlWriter consumes the RoutesProvider interface and works in both
+ * Rampage (without legacy bootstrap) and legacy flows. This class remains
+ * for existing callers wired through _bootstrap() in Application.php.
+ */
 class UrlGenerator
 {
     private Utils $utils;
